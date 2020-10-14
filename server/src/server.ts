7717b59fefcddcd8d70193a417c69f6289ff4446
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-10-14 19:43:37 +0000

    server/Add create Orphanage Repository & create orphanage route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,10 @@
 import express from 'express';
-
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', (request, response) => {
-  return response.json({ message: "Hello World from express!" });
-});
+app.use(routes);
 
 app.listen(8888);
